b065d9fcfb94502adc0229948b356330b901aa8d
morteza-jamali <mortezajamali4241@gmail.com>
2020-10-14 23:08:46 +0330

    Metro4 framework added 1

--- desktop/src/app/app.module.ts
+++ desktop/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { DesktopComponent } from './desktop/desktop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    DesktopComponent
   ],
   imports: [
     BrowserModule,
---
+++ desktop/src/app/desktop/desktop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop',
+  templateUrl: './desktop.component.html',
+  styleUrls: ['./desktop.component.sass']
+})
+export class DesktopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
