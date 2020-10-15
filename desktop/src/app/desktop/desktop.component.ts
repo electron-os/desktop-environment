7db9442e0c4488b93758ed6b5256ae890b79175f
morteza-jamali <mortezajamali4241@gmail.com>
2020-10-16 00:55:58 +0330

    window component added !

--- desktop/src/app/desktop/desktop.component.ts
+++ desktop/src/app/desktop/desktop.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MetroService } from '../metro.service';
 
 @Component({
   selector: 'app-desktop',
   templateUrl: './desktop.component.html',
   styleUrls: ['./desktop.component.sass']
 })
+
 export class DesktopComponent implements OnInit {
 
-  constructor() { }
+  constructor(private metroService : MetroService) { }
 
   ngOnInit(): void {
+    this.metroService.getMetro().init();
   }
 
 }
