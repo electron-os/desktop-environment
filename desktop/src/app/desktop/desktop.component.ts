f06da9e3f0eef68e13af2cb3f09c9988c7726846
morteza-jamali <mortezajamali4241@gmail.com>
2020-10-23 13:22:18 +0330

    micon icon pack added

--- desktop/src/app/desktop/desktop.component.ts
+++ desktop/src/app/desktop/desktop.component.ts
@@ -8,11 +8,37 @@ import { MetroService } from '../metro.service';
 })
 
 export class DesktopComponent implements OnInit {
+  metro: any;
+  $: any;
+  componentsStatus = {
+    startMenuSide: false ,
+    startMenu: false
+  };
+  powerbuttonPopOverContent: string;
 
-  constructor(private metroService : MetroService) { }
-
-  ngOnInit(): void {
-    this.metroService.getMetro().init();
+  constructor(private metroService : MetroService) {
+    this.metro = this.metroService.getMetro();
+    this.$ = this.metro.$();
+    this.setPowerbuttonPopOverContent();
   }
 
+  ngOnInit(): void {
+    this.metro.init();
+  }
+
+  expandStartMenuSide(status: boolean): void {
+    this.componentsStatus.startMenuSide = status;
+  }
+
+  toggleComponent(name: string): void {
+    this.componentsStatus[name] = !this.componentsStatus[name];
+  }
+
+  setPowerbuttonPopOverContent(): void {
+    this.powerbuttonPopOverContent = "<div class='powerButtonPopOver'><ul>" +
+    "<li><a><i class='mi mi-PowerButton icon'></i>Shut down</a></li>" +
+    "<li><a><i class='mi mi-PowerButton icon'></i>Restart</a></li>" +
+    "<li><a><i class='mi mi-PowerButton icon'></i>Sleep</a></li>" +
+    "</ul></div>";
+  }
 }
